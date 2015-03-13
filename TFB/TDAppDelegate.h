38b0d37976cf24976c746898b70fdf0fc9760990
Nothing <Nothing@Nothing.local>
2015-03-13 10:50:43 +0800

    Initial Commit

---
+++ TFB/TDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDAppDelegate.h
+//  TFB
+//
+//  Created by Nothing on 15/3/13.
+//  Copyright (c) 2015年 TD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
